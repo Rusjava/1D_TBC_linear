a3c2c899cc50e2b831c8fa8b22d5d0a9fad92db9
rusjava <ruslik00@gmail.com>
2018-11-26 13:11:03 +0300

    First commit

---
+++ 1D_TBC_linear.py
@@ -0,0 +1,9 @@
+# Implementation of the unconditionally stable TBC for linear potential
+
+import math
+import cmath
+import numpy as np
+import io
+import matplotlib.pyplot as plt
+
+imagefilename = "E:\\Python\\Results\\1D_stable_colorplot.png"  # ------------ The name of the image file to save results to
