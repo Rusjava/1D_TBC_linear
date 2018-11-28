8763f06d62a5f7c1ab26d46bb8050948b904e973
rusjava <ruslik00@gmail.com>
2018-11-28 13:58:33 +0300

    Expanded auxiliary functions. Modified the code to support linear potential in the Schrodiger equation. Some minor improvements.

--- 1D_TBC_linear.py
+++ 1D_TBC_linear.py
@@ -1,4 +1,4 @@
-# Implementation of the unconditionally stable TBC for linear potential
+# Implementation of the unconditionally stable TBC for Schrodinger equation with linear oscillating potential
 
 import math
 import cmath
@@ -11,50 +11,32 @@ if __name__ == '__main__':
 
     imagefilename = "E:\\Python\\Results\\1D_stable_colorplot.png"  # ------------ The name of the image file to save results to
 
-    lam = 0.1  # -----------------Wavelength, nm
-    rho = 0.00054
-
-    f1Si = 14.142
-    f2Si = 0.141
-
-    den1 = 2.3  # --------------------------------------SiO2
-    M1 = 32.0
-
-    delta1 = den1 * rho / M1 * lam**2 * f1Si
-    beta1 = den1 * rho / M1 * lam**2 * f2Si
-
     RMIN = 30  # ------------------------Gap semi-thickness
     RMAX = 100  # ------------Maximum x
     angle = 0  # ------------------------Incidence angle, mrad
-    ZMAX = 1e6  # ----------------Waveguide length, nm
-    R = 1e6;  # ---------------------------Curvature radius
+    ZMAX = 1e3  # ----------------Waveguide length, nm
 
     h = 0.5  # ----------------------------- Transversal step
-    tau_int = 1e3  # ----------------------------- Longitudinal step
+    tau_int = 0.5  # ----------------------------- Longitudinal step
     sprsn = 2  # ----------------------------ARRAY thinning(long range)
     sprsm = 1  # ----------------------------ARRAY thinning
 
-
-    alp1 = -delta1 + 1j * beta1  # Complex dielectric constant definition
+    alp1 = 0  # The potential well depth
     alp0 = 0
 
-    waveguide = 1; #  -------------------------key: 0 -- rod, 1 -- waveguide
-
-    # Vacuum outside
-    if waveguide == 0:
-        k = 2 * math.pi / lam
-    # Vacuum inside
-    else:
-        k = cmath.sqrt(1 + alp1) * 2 * math.pi / lam
-        alp1 = -alp1 / (1 + alp1)
-
-    aa = 2*k**2/R;  # ------------------------------- a parameter
+    kappa = 0  # ------------------------------- The external field strength
+    K = 1  # The spatial frequency of the initial condition
+    fq = 0.01  # Oscillation frequency
+    model = 0  # The initial probability model
 
     MMAX = int(round(2. * RMAX / h))
     muMAX = math.floor((MMAX + 2) / sprsm)
     MMIN = int(round((RMAX - RMIN) / h))
     MMIN2 = int(round((RMAX + RMIN) / h))
     NMAX = int(round(ZMAX / tau_int))
+    WAIST = RMIN/2  # Gaussian beam waist
+
+    # Sparsing parameters
     if sprsn != 1:
         nuMAX = math.floor(NMAX / sprsn) + 1
     else:
@@ -63,14 +45,14 @@ if __name__ == '__main__':
     r = np.r_[0:MMAX+2] * h
     z = tau_int * np.r_[0:NMAX]
 
-    # -----------------------------------------------Epsilon(r)
+    # -----------------------------------------------Potential(r)
 
-    # ------------------------------PLANE WAVE
-    u0 = np.exp(1j * k * math.sin(angle) * r)
-
-    # ---------------------------------------GAUSSIAN
-    # WAIST = RMIN
-    # u0 = np.exp(-(r - RMAX)**2 / WAIST**2 + 1j * k * math.sin(angle) * r)
+    if model == 0:
+        # ------------------------------PLANE WAVE
+        u0 = np.exp(1j * K * math.sin(angle) * r)
+    elif model == 1:
+        # ---------------------------------------GAUSSIAN
+        u0 = np.exp(-(r - RMAX)**2 / WAIST**2 + 1j * K * math.sin(angle) * r)
 
     # -------------------------------------
     u = np.copy(u0)
@@ -90,16 +72,16 @@ if __name__ == '__main__':
     rplot = r[sprsm * np.r_[0:muMAX]]
     P = np.ones(MMAX + 2,dtype=complex)
     Q = np.ones(MMAX + 2,dtype=complex)
-    c1 = (k*h)**2
+    c1 = h**2/4
     alp0 *= c1
     alp1 *= c1
 
     # Initializing sparse field amplitude array
     nuu = 1
     zplot[0] = z[0]
-    uplot[0:muMAX, 0] = np.exp(1j * k * z[0]) * u[sprsm * np.r_[0:muMAX]]
+    uplot[0:muMAX, 0] = u[sprsm * np.r_[0:muMAX]]
 
-    c0 = 4. * 1j * k * h**2 / tau_int
+    c0 = 2 * 1j * h**2 / tau_int
     ci = 2. - c0
     cci = 2. + c0
 
@@ -108,16 +90,15 @@ if __name__ == '__main__':
     phi = -1. / 2. - (-1.)**np.r_[0:NMAX+1] + ((-1.)**np.r_[0:NMAX+1]) / 2. * ((1. + c0 / 4.) / (1. - c0 / 4.))**np.r_[1:NMAX+2]
     beta[0] = phi[0]
     gg[0] = 1
-    qq = 1j * 2. * cmath.sin(k * math.sin(angle) * h) / beta0
-    yy = cmath.cos(k * math.sin(angle) * h)
-    mm = cmath.exp(1j * k * math.sin(angle) * h)
+    qq = -cmath.sin(K * math.sin(angle) * h) / cmath.sqrt(c0 - c0**2 / 4.)
+    yy = cmath.cos(K * math.sin(angle) * h)
 
     for cntn in np.r_[1:NMAX]:
 
-    #  Dielectric constants with the bent term
-        alp[0:MMAX + 2] = aux.d_function(r, RMAX, RMIN, alp1, alp0)
+        #  Dielectric constants with the bent term
+        alp[0:MMAX + 2] = aux.step_p(r + 2*kappa/(fq**2)*math.sin(fq*tau_int*cntn), RMAX, RMIN, alp1, alp0)
 
-    # Top and bottom boundary conditions
+        # Top and bottom boundary conditions
         gg[cntn] = ((c0 + 2. - 2. * yy) / (c0 - 2. + 2. * yy))**cntn
 
         SS = -np.dot(ubottom[0:cntn], beta[0:cntn]) - ((qq-1)*gg[cntn] - np.dot(gg[0:cntn], beta[0:cntn])) * ubottom[0]
@@ -125,7 +106,7 @@ if __name__ == '__main__':
 
         beta[cntn] = (np.dot(phi[0:cntn], beta[0:cntn]) + phi[cntn])/(cntn+1)
 
-    # Initial condition at the bottom
+        # Initial condition at the bottom
         c = ci - alp[1]
         cconj = cci - alp[1]
         d = u[2] - cconj * u[1] + u[0]
@@ -133,7 +114,7 @@ if __name__ == '__main__':
         P[0] = -(c - beta0) / 2.
         Q[0] = -(d - beta0 * SS) / 2.
 
-    # Preparation for marching
+        # Preparation for marching
         for cntm in np.r_[1:MMAX+1]:
             c = ci - alp[cntm]
             cconj = cci - alp[cntm]
@@ -142,21 +123,23 @@ if __name__ == '__main__':
             P[cntm] = -1. / (c + P[cntm-1])
             Q[cntm] = -(Q[cntm-1] + d) * P[cntm]
 
-    # Initial condition at the top
+        # Initial condition at the top
         u[MMAX + 1] = (beta0 * SS1 + Q[MMAX-1] - (P[MMAX-1] + beta0) * Q[MMAX]) / (1. - (beta0 + P[MMAX-1]) * P[MMAX])
 
-    # Solving the system
+        # Solving the system
         for cntm in np.r_[MMAX+1:0: -1]:
             u[cntm-1] = Q[cntm-1] - P[cntm-1] * u[cntm]
 
-    # Preserving boundary values
+        # Preserving boundary values
         utop[cntn] = u[MMAX]
         ubottom[cntn] = u[1]
 
-    # Sparsing
+        # Sparsing
         if (cntn-1) / sprsn - math.floor((cntn-1) / sprsn) == 0:
             zplot[nuu] = z[cntn-1]
-            uplot[0:muMAX, nuu] = np.exp(1j * k * z[cntn-1]) * u[sprsm * np.r_[0:muMAX]]
+            #  Multiplying by the phase factor
+            tmp = cmath.exp(-1j*kappa**2/fq**2/2*tau_int*cntn*fq+1j*3/4*kappa**2/fq**3*math.sin(tau_int*cntn*fq))
+            uplot[0:muMAX, nuu] = tmp*np.exp(-1j*kappa/fq*rplot*math.cos(tau_int*cntn*fq))*u[sprsm * np.r_[0:muMAX]]
             nuu = nuu + 1
         # Printing the execution progress
         progress = int(round(1.*(cntn-1) / NMAX * 100))
@@ -167,8 +150,8 @@ if __name__ == '__main__':
 
     # Preparing the title string
     buf = io.StringIO()
-    buf.write("|u|: $\lambda =$ %1.2f nm   $XMAX =$ %4.2f $\mu$m  $XMIN =$ %4.2f $\mu$m  $ZMAX =$ %5.0f $\mu$m " \
-              % (lam, RMIN * 1e-3, RMAX * 1e-3, ZMAX * 1e-3))
+    buf.write("|u|: angle = %1.2f,  $XMAX =$ %4.2f $\mu$m,  $XMIN =$ %4.2f $\mu$m,  $ZMAX =$ %5.0f $\mu$m" \
+              % (angle, RMIN * 1e-3, RMAX * 1e-3, ZMAX * 1e-3))
 
     # Plotting the field amplitude in a color chart
     fig, gplot = plt.subplots()
--- aux_functions.py
+++ aux_functions.py
@@ -2,10 +2,11 @@
 
 import numpy as np
 
-#  Dielectric function definition: x is a ndarray
+#  Dielectric function definitions
 
-def d_function(x, x0, rad, v1, v2):
-    """x is a ndarray; x0, rad are complex numbers; v1, v2 are real numbers"""
+
+def parabolic_p(x, x0, rad, v1, v2):
+    """Parabolic potential. x is a ndarray; x0, rad are complex numbers; v1, v2 are real numbers"""
     res = np.zeros(x.size,dtype=complex)
     for i in np.r_[0:x.size]:
         if abs(x[i]-x0)>rad:
@@ -13,3 +14,20 @@ def d_function(x, x0, rad, v1, v2):
         else:
             res[i] = (v1-v2)*(1.-((x[i]-x0)/rad)**2) + v2
     return res
+
+
+def step_p(x, x0, rad, v1, v2):
+    """Step-like potential. x is a ndarray; x0, rad are complex numbers; v1, v2 are real numbers"""
+    res = np.zeros(x.size,dtype=complex)
+    for i in np.r_[0:x.size]:
+        if abs(x[i]-x0) > rad:
+            res[i] = v2
+        else:
+            res[i] = v1
+    return res
+
+
+# Temporal oscillations
+def sin_temp(t, fq):
+    """Oscillating potential field. t is a ndarray; fq (frequancy) is real number"""
+    return np.sin(fq*t)
